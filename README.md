5b6a98a07abd554dd28636fa43d41b425c00bda6
Ayush <ayushg1500@gmail.com>
2024-09-29 03:00:52 +0530

    this is 1st commit in new_branch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # githubtutorial
 This repository was made during git and github tutorial
+This is forked repo 
+it has been created during git and github tutorial
--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
+def fibonnaci(n):
+    if(n==0):
+        return 0
+    if(n==1):
+        return 1
+    return fibonnaci(n-1)+fibonnaci(n-2)
+
 a=int(input("Enter 1st number:"))
 b=int(input("Enter 2nd number:"))
 print(a+b)
 print("Hello This is Git and GitHub tutorial")
+print(fibonnaci(a))
 
